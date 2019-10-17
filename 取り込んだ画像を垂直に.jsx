a1ff6d4a506c6c8231c9f763309f80db26221106
Junsund <56618874+Junsund@users.noreply.github.com>
2019-10-17 13:18:44 +0900

    Update 取り込んだ画像を垂直に.jsx

--- 取り込んだ画像を垂直に.jsx
+++ 取り込んだ画像を垂直に.jsx
@@ -1,3 +1,4 @@
+app.executeMenuCommand("selectall");
 sel = activeDocument.selection;
 selObj = activeDocument.pathItems;
 
@@ -9,16 +10,13 @@ for (var i=0; i<selObj.length; i++)
     var p2x = selObj[i].pathPoints[1].anchor[0];
     var p2y = selObj[i].pathPoints[1].anchor[1];
         if(p1x>p2x){
-        
         lObj = activeDocument.pathItems.add();
         lObj.setEntirePath([[p2x,p2y],[p1x,p1y]]);
         lObj.filled = false; // 塗りなし
         lObj.stroked = true; // 線あり
-        lObj.strokeWidth = linesize;
         lObj.selected = true; //選択する
         lObj.name = "centerLine"; //名前
         selObj[i].remove();
-        
     }
     else
     {
@@ -29,12 +27,20 @@ for (var i=0; i<selObj.length; i++)
         lObj.setEntirePath([[p1x,p1y],[p2x,p2y]]);
         lObj.filled = false; // 塗りなし
         lObj.stroked = true; // 線あり
-        lObj.strokeWidth = linesize;
         lObj.selected = true; //選択する
         lObj.name = "centerLine"; //名前
         selObj[i].remove();
         
     }
+}
+
+app.executeMenuCommand("selectall");
+for (var i=0; i<selObj.length; i++){
+    selObj[i].selected = true;
+    var p1x = selObj[i].pathPoints[0].anchor[0];
+    var p1y = selObj[i].pathPoints[0].anchor[1];
+    var p2x = selObj[i].pathPoints[1].anchor[0];
+    var p2y = selObj[i].pathPoints[1].anchor[1];
 }
 
 gObj = activeDocument.groupItems.add();
@@ -46,7 +52,7 @@ for (var i=0; i<sel.length; i++)
 if (p1x == p1y){}
 else
 {
-    var d = Math.atan2(Math.abs(p1y-p2y),p2x-p1x);
+    var d = Math.atan2(Math.abs(p2y-p1y),(p1x-p2x));
     gsel = activeDocument.groupItems;
     for (i=0; i<gsel.length; i++){
         gsel[i].rotate((d* ( 180 / Math.PI ))-90,true,false,false,false,Transformation.CENTER);
